2731e7f00757ccfe9f64a76f46b5c7df910879b3
aditiyakashyap <kashyap.aditiya27@gmail.com>
2026-03-11 00:11:58 +0530

    Create data.ts

---
+++ src/lib/data.ts
@@ -0,0 +1,32 @@
+export const properties = [
+  {
+    id: '1',
+    title: 'Luxury Builder Floor in South City',
+    type: 'Builder Floor',
+    price: '$450,000',
+    location: 'South City, Downtown',
+    isPromoted: true,
+    image: 'https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?w=800&q=80',
+    seller: 'Elite Builders Inc.'
+  },
+  {
+    id: '2',
+    title: 'Green Valley Housing Society',
+    type: 'Housing Project',
+    price: 'From $250,000',
+    location: 'West End Suburbs',
+    isPromoted: false,
+    image: 'https://images.unsplash.com/photo-1545324418-cc1a3fa10c00?w=800&q=80',
+    seller: 'GreenTech Developers'
+  },
+  {
+    id: '3',
+    title: 'Downtown Commercial Hub',
+    type: 'Commercial',
+    price: '$1,200,000',
+    location: 'Business District',
+    isPromoted: true,
+    image: 'https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?w=800&q=80',
+    seller: 'Commercial Prime'
+  }
+];
